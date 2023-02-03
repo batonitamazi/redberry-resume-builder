61fdf37dbca028edf11522cbe09f5abc406d72ad
btntazo <tamazimirianashvili@gmail.com>
2023-02-03 16:54:57 +0400

    add: formik forms

--- resume-builder/src/pages/ExperiencesPage.js
+++ resume-builder/src/pages/ExperiencesPage.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { Field, Form, Formik } from "formik";
+import { validationSchemas } from "../helpers/validations";
 
 
 function ExperiencesPage() {
   const navigate = useNavigate()
+  const getValues = (values) => {
+    console.log(values);
+  };
   return (
     <div>ExperiencesPage
       <button onClick={() => navigate('/general-information')}>
@@ -12,6 +17,44 @@ function ExperiencesPage() {
       <button onClick={() => navigate('/education')}>
         შემდეგი
       </button>
+      <Formik
+        validationSchema={validationSchemas["experienceInformation"]}
+        initialValues={{
+          position: "",
+          employer: "",
+          startDate: "",
+          endDate: "",
+          description: "",
+        }}
+        onSubmit={getValues}
+      >
+        {({ errors, touched, handleSubmit, handleChange }) => (
+          <Form
+            onSubmit={(values) => {
+              console.log(errors);
+              handleSubmit(values);
+            }}  
+              >
+            <Field name="position"  onChange={handleChange}/>
+            {errors.position && touched.position ? <div>{errors.position}</div> : null}
+            <Field name="surname"  onChange={handleChange}/>
+            {errors.employer && touched.employer ? (
+              <div>{errors.employer}</div>
+            ) : null}
+            <Field name="startDate"  type="date" onChange={handleChange}/>
+            {errors.startDate && touched.startDate ? (
+              <div>{errors.startDate}</div>
+            ) : null}
+            <Field name="endDate"  onChange={handleChange}/>
+            {errors.endDate && touched.endDate ? (
+              <div>{errors.endDate}</div>
+            ) : null}
+            <Field name="description" type="description"  onChange={handleChange}/>
+            {errors.description && touched.description ? <div>{errors.description}</div> : null}
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
     </div>
   )
 }
--- resume-builder/src/pages/GeneralInfoPage.js
+++ resume-builder/src/pages/GeneralInfoPage.js
@@ -1,16 +1,76 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, {useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Field, Form, Formik } from "formik";
+import { validationSchemas } from "../helpers/validations";
+
+const initialValues = {
+  name: "",
+  surname: "",
+  image: "",
+  aboutMe: "",
+  email: "",
+  phone: "",
+};
 
 function GeneralInfoPage() {
-  const navigate = useNavigate()
+  
+  const [formValues, setFormValues] = useState(initialValues);
+  const navigate = useNavigate();
+  const getValues = (values) => {
+    setFormValues(values);
+  };
   return (
     <div>
       GeneralInfoPage
-      <button onClick={() => navigate("/experience")}>
-        შემდეგი
-      </button>
+      <button onClick={() => navigate("/experience")}>შემდეგი</button>
+      <Formik
+        validationSchema={validationSchemas["generalInformation"]}
+        initialValues={initialValues}
+        onSubmit={getValues}
+      >
+        {({ errors, touched, handleSubmit, handleChange, values }) => {
+          setFormValues(values)
+          
+          return (
+            <Form
+              onSubmit={(values) => {
+                console.log(errors);
+                handleSubmit(values);
+              }}
+            >
+              <Field name="name" onChange={handleChange} />
+              {errors.name && touched.name ? <div>{errors.name}</div> : null}
+              <Field name="surname" onChange={handleChange} />
+              {errors.surname && touched.surname ? (
+                <div>{errors.surname}</div>
+              ) : null}
+              <Field name="image" type="file" onChange={handleChange} />
+              {errors.surname && touched.surname ? (
+                <div>{errors.image}</div>
+              ) : null}
+              <Field name="aboutMe" onChange={handleChange} />
+              {errors.aboutMe && touched.aboutMe ? (
+                <div>{errors.aboutMe}</div>
+              ) : null}
+              <Field name="email" type="email" onChange={handleChange} />
+              {errors.email && touched.email ? <div>{errors.email}</div> : null}
+              <Field name="phone" onChange={handleChange} />
+              {errors.phone && touched.phone ? <div>{errors.phone}</div> : null}
+              <button type="submit">Submit</button>
+            </Form>
+          );
+        }}
+      </Formik>
+      <div>
+        <p>{formValues.name}</p>
+        <p>{formValues.surname}</p>
+        {formValues.image ? <img src={formValues.image} alt="employee img" /> : null}
+        <p>{formValues.email}</p>
+        <p>{formValues.aboutMe}</p>
+        <p>{formValues.phone}</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default GeneralInfoPage
+export default GeneralInfoPage;
